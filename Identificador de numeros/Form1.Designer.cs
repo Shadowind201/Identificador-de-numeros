673f3d189b1326b7c6e4ca20ae208bb9dea17fae
Shadowind201 <enzocarnier@gmail.com>
2024-06-12 20:16:50 -0300

    Adicionar arquivos de projeto.

---
+++ Identificador de numeros/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Identificador_de_numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.txtNum4 = new System.Windows.Forms.TextBox();
+            this.txtNum5 = new System.Windows.Forms.TextBox();
+            this.txtMaior = new System.Windows.Forms.TextBox();
+            this.txtMenor = new System.Windows.Forms.TextBox();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(91, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Indentificador de numeros";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(164, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Forneça 5 números";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(43, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number 1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(114, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number 2";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(188, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Number 3";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(267, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Number 4";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(347, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Number 5";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label8.Location = new System.Drawing.Point(94, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Maior Número";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label9.Location = new System.Drawing.Point(267, 257);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Menor Número";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(46, 145);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(50, 20);
+            this.txtNum1.TabIndex = 9;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(117, 145);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(50, 20);
+            this.txtNum2.TabIndex = 10;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Location = new System.Drawing.Point(191, 145);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(50, 20);
+            this.txtNum3.TabIndex = 11;
+            // 
+            // txtNum4
+            // 
+            this.txtNum4.Location = new System.Drawing.Point(270, 145);
+            this.txtNum4.Name = "txtNum4";
+            this.txtNum4.Size = new System.Drawing.Size(50, 20);
+            this.txtNum4.TabIndex = 12;
+            // 
+            // txtNum5
+            // 
+            this.txtNum5.Location = new System.Drawing.Point(350, 145);
+            this.txtNum5.Name = "txtNum5";
+            this.txtNum5.Size = new System.Drawing.Size(50, 20);
+            this.txtNum5.TabIndex = 13;
+            // 
+            // txtMaior
+            // 
+            this.txtMaior.Location = new System.Drawing.Point(97, 273);
+            this.txtMaior.Name = "txtMaior";
+            this.txtMaior.Size = new System.Drawing.Size(70, 20);
+            this.txtMaior.TabIndex = 14;
+            // 
+            // txtMenor
+            // 
+            this.txtMenor.Location = new System.Drawing.Point(270, 273);
+            this.txtMenor.Name = "txtMenor";
+            this.txtMenor.Size = new System.Drawing.Size(74, 20);
+            this.txtMenor.TabIndex = 15;
+            // 
+            // Calcular
+            // 
+            this.Calcular.BackColor = System.Drawing.Color.SeaGreen;
+            this.Calcular.Location = new System.Drawing.Point(164, 201);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(98, 30);
+            this.Calcular.TabIndex = 16;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = false;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(438, 354);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.txtMenor);
+            this.Controls.Add(this.txtMaior);
+            this.Controls.Add(this.txtNum5);
+            this.Controls.Add(this.txtNum4);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.TextBox txtNum4;
+        private System.Windows.Forms.TextBox txtNum5;
+        private System.Windows.Forms.TextBox txtMaior;
+        private System.Windows.Forms.TextBox txtMenor;
+        private System.Windows.Forms.Button Calcular;
+    }
+}
+
---
+++ Identificador de numeros/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Identificador_de_numeros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {   
+            //Converte os textos das TextBoxes para Números do tipo double
+            double num1 = Convert.ToDouble(txtNum1.Text);
+            double num2 = Convert.ToDouble(txtNum2.Text);
+            double num3 = Convert.ToDouble(txtNum3.Text);
+            double num4 = Convert.ToDouble(txtNum4.Text);
+            double num5 = Convert.ToDouble(txtNum5.Text);
+
+            //Armazena os números em um array para fácil manipulação
+            double[] numbers = { num1, num2, num3, num4, num5, };
+
+            //Iniciaza 
+            double max = numbers[0];
+            double min = numbers[0];
+
+            foreach (double num in numbers)
+            {
+                if (num > max) // Se o número atual for maior que max, atualiza max
+                {
+                    max = num;
+                }
+                if (num < min) // Se o número atual for menor que min, atuliza min
+                {
+                    min = num;
+                }
+            }
+            txtMaior.Text = max.ToString(); 
+            txtMenor.Text = min.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
